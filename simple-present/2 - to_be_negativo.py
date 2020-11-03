4d7374ef5ccc4960c068f96a7344c0d1c3dbbd1e
Inglês com Python <73453491+inglescompython@users.noreply.github.com>
2020-11-03 12:51:06 -0300

    Algoritmo 2 - To Be Negativo

---
+++ simple-present/2 - to_be_negativo.py
@@ -0,0 +1,35 @@
+###############################################################################
+#
+# Algoritmos criados para fins educativos no projeto Inglês com Python
+# Nos sigam no Instagram e Facebook @inglescompython 
+# Aula 02 = Algoritmo "Verbo To Be" Forma Negativa
+#
+###############################################################################
+
+def to_be_negativo(pronome):
+    
+    excecao = ['I']
+    plural = ['You','They','We']
+    singular = ['He','She','It']
+
+    # You é tanto pessoa do singular (você) como plural (vocês)
+    # mas a conjugação do verbo to be é o mesmo: "You are"
+
+    if(pronome in excecao):
+        print(pronome + " am"+" not")
+    elif(pronome in singular):
+        print(pronome + " are"+" not")
+    elif(pronome in plural):
+        print(pronome + " is"+ " not")
+    else:
+        print("Pronome Desconhecido")
+
+todos_pronomes = ["I","You","He","She","It","We","You","They"]
+
+for p in todos_pronomes:
+    to_be_negativo(p)
+
+#Chamadas de função
+#to_be_negativo("I")
+#to_be_negativo("He")
+#to_be_negativo("They")
