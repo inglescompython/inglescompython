416d62ba3c13f4a2866201f995a44732bf8ec00e
Inglês com Python <73453491+inglescompython@users.noreply.github.com>
2020-11-12 12:20:34 -0300

    Algoritmo 05 - Auxiliar Do e Does Interrogativo

---
+++ simple-present/verbo_auxiliar_interrogativo.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+# Algoritmos criados para fins educativos no projeto Inglês com Python
+# Nos sigam no Instagram e Facebook @inglescompython 
+# Aula 05 = Algoritmo Uso do Auxiliar "Do" e "Does" Interrogativo
+#
+###############################################################################
+
+def verbo_auxiliar_interrogativo(pronome):
+
+    terceira_pessoa_singular = ['He','She','It']
+
+    if(pronome in terceira_pessoa_singular):
+        return "Does "+pronome+" ..?"
+    else:
+        return "Do "+pronome+" ...?"
+
+todos_pronomes = ["I","You","He","She","It","We","You","They"]
+
+for pronome in todos_pronomes:
+    print(verbo_auxiliar_interrogativo(pronome))
