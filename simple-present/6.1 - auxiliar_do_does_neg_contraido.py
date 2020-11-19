1d33a5a7a6820b549562ab1512247a1dab5b17ce
Inglês com Python <73453491+inglescompython@users.noreply.github.com>
2020-11-19 10:54:21 -0300

    Algoritmo 6.1 - Auxilar Do e Does Neg. Contraído

--- simple-present/6.1 - auxiliar_do_does_neg_contraido.py
+++ simple-present/6.1 - auxiliar_do_does_neg_contraido.py
@@ -1,9 +1,12 @@
-def auxiliar_do_does_neg_contraido(verbo):
+def auxiliar_do_does_negativo_contraido(verbo):
 
     terceira_pessoa_singular =  ['He','She','It']
     negativo = "not"
     negativo_contraido = negativo.replace("o","'")
-    
+
+    #Substitui no 'Not' o 'o' por 'aspa simples' => n[o]t => n[']t
+    # e concatena na frente do Auxiliar: Do+n't => Don't, Does+n't => Doesn't
+
     if(pronome in terceira_pessoa_singular):
         return pronome+' does'+negativo_contraido
     else:
@@ -13,4 +16,4 @@ todos_pronomes = ['I','You','He','She',
                  'It','We','You','They']
 
 for pronome in todos_pronomes:
-    print(auxiliar_do_does_neg_contraido(pronome))
+    print(auxiliar_do_does_negativo_contraido(pronome))
