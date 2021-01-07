97a9ada7284cb8c8ade3158eb796a2163b2b5f8c
Inglês com Python <73453491+inglescompython@users.noreply.github.com>
2021-01-07 15:47:54 -0300

    Algoritmo 1 - Auxiliar Did Interrogativo

---
+++ simple-past/1 - auxiliar_did_interrogativo.py
@@ -0,0 +1,8 @@
+def auxiliar_did_interrogativo(pronome):
+    return "Did " + pronome
+
+pronomes = ['I','You','He','She',
+           'It','We','You','They']
+
+for pronome in pronomes:
+    print(auxiliar_did_interrogativo(pronome))
