442154d2fe7f1936891de12ed0cb187395999201
Inglês com Python <73453491+inglescompython@users.noreply.github.com>
2021-01-07 16:50:35 -0300

    Algoritmo 9 - Auxiliar Did Interrogativo

--- simple-past/1 - auxiliar_did_interrogativo.py
+++ simple-past/1 - auxiliar_did_interrogativo.py
@@ -1,3 +1,11 @@
+###############################################################################
+#
+#   Algoritmos criados para fins educativos no projeto Inglês com Python
+#   Nos siga no Instagram e Facebook: @inglescompython 
+#   Aula 09 - Algoritmo "Auxiliar Did Forma Interrogativa"
+#
+###############################################################################
+
 def auxiliar_did_interrogativo(pronome):
     return "Did " + pronome
 
