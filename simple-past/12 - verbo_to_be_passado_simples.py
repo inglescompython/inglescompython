d76e00738f92b76f99b9bfaddce0b3ac71cede3b
Inglês com Python <73453491+inglescompython@users.noreply.github.com>
2021-02-04 18:40:03 -0300

    Algoritmo 12 - Verbo To Be Passado Simples

---
+++ simple-past/12 - verbo_to_be_passado_simples.py
@@ -0,0 +1,17 @@
+def verbo_to_be_passado_simples(pronome):
+
+    singular = ["I","He","She","It"]
+    plural = ["You","We","They"]
+
+    #You é tanto singular como plural, porem se
+    #usa o verbo do plural em ambos. You were
+
+    if(pronome in singular):
+        return pronome+" was"
+    elif(pronome in plural):
+        return pronome+" were"
+
+pronomes = ["I","You","He","She","It","We","You","They"]
+
+for pronome in pronomes:
+    print(verbo_to_be_passado_simples(pronome))
