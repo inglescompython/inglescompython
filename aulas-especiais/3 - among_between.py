42761a0c9cb4bc0f5969bc45ac7c6a390c70f028
Inglês com Python <73453491+inglescompython@users.noreply.github.com>
2021-02-18 09:37:18 -0300

    Uso do Among vs Between

---
+++ aulas-especiais/3 - among_between.py
@@ -0,0 +1,10 @@
+def among_between(quantidade_pessoas):
+    if(quantidade_pessoas == 2):
+        print("Between")
+    elif(quantidade_pessoas > 2):
+        print("Among")
+    else:
+        print("Quantidade Invalida")
+
+among_between(2)
+among_between(3)
