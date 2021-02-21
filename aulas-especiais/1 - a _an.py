9c60e459439f9f5d1f73089f67a65b07a3531aa3
Inglês com Python <73453491+inglescompython@users.noreply.github.com>
2021-02-21 11:04:02 -0300

    Algoritmo 1 - Uso do An e A

--- aulas-especiais/1 - a _an.py
+++ aulas-especiais/1 - a _an.py
@@ -1,6 +1,7 @@
 def a_an(palavra):
     vogais = ['a','e','i','o','u']
-    if(palavra[0] in vogais): 
+    
+    if(palavra[0] in vogais):
         print("an "+palavra)
     else:
         print("a "+palavra)
@@ -9,3 +10,6 @@ a_an('apple')
 a_an('bird')
 a_an('umbrella')
 a_an('house')
+
+#Se a primeira letra for vogal ou tiver som de vogal se usa An
+#Se a primeira letra for consoante ou tiver som de consoante se usa A
