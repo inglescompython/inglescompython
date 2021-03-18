1d6ffcbc27527c5d61d8a3a3235445a6e3d2c229
Inglês com Python <73453491+inglescompython@users.noreply.github.com>
2021-03-18 14:08:33 -0300

    Update 1- conjugacao_verbal_futuro_simples.py

--- simple-future/1- conjugacao_verbal_futuro_simples.py
+++ simple-future/1- conjugacao_verbal_futuro_simples.py
@@ -1,9 +1,10 @@
 def conjugacao_verbal_futuro_simples(pronome):
-return pronome + " will" + " be" 
+  return pronome + " will" + " be" 
 
   
 pronomes = ["I", "You", "He", "She", 
            "It", "We", "You", "They"]
 
 
-for pronome in pronomes:                                                 print(conjugacao_verbal_futuro_simples(pronome))
+for pronome in pronomes:
+  print(conjugacao_verbal_futuro_simples(pronome))
