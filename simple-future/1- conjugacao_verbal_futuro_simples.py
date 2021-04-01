790f2a743b1ce881e84473ea448de068686e1292
Inglês com Python <73453491+inglescompython@users.noreply.github.com>
2021-04-01 15:46:14 -0300

    1 - Verbo To Be Futuro Simples

--- simple-future/1- conjugacao_verbal_futuro_simples.py
+++ simple-future/1- conjugacao_verbal_futuro_simples.py
@@ -1,10 +1,9 @@
-def conjugacao_verbal_futuro_simples(pronome):
+def verbo_to_be_futuro_simples(pronome):
   return pronome + " will" + " be" 
 
   
 pronomes = ["I", "You", "He", "She", 
            "It", "We", "You", "They"]
 
-
 for pronome in pronomes:
-  print(conjugacao_verbal_futuro_simples(pronome))
+  print(verbo_to_be_futuro_simples(pronome))
