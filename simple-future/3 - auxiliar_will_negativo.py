f07b6ac5df93be3fd2acae6d2c186ff4bd4b9a77
Inglês com Python <73453491+inglescompython@users.noreply.github.com>
2021-04-15 09:54:37 -0300

    Algoritmo 16 - Auxiliar Will Forma Negativa

--- simple-future/3 - auxiliar_will_negativo.py
+++ simple-future/3 - auxiliar_will_negativo.py
@@ -1,3 +1,11 @@
+###############################################################################
+#
+#   Algoritmos criados para fins educativos no projeto Inglês com Python
+#   Nos siga no Instagram e Facebook: @inglescompython 
+#   Aula 01 - Algoritmo "Auxiliar Will Forma Negativa"
+#
+###############################################################################
+
 def auxiliar_will_negativa(pronome):
     return pronome + " will" + " not"
 
