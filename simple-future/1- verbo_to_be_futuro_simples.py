ccbddef6b73fa5ec3e17ccfe24f294dde40eede0
Inglês com Python <73453491+inglescompython@users.noreply.github.com>
2021-04-15 09:52:59 -0300

    Algoritmo 14 - Verbo To Be Futuro Simples

--- simple-future/1- verbo_to_be_futuro_simples.py
+++ simple-future/1- verbo_to_be_futuro_simples.py
@@ -1,3 +1,11 @@
+###############################################################################
+#
+#   Algoritmos criados para fins educativos no projeto Inglês com Python
+#   Nos siga no Instagram e Facebook: @inglescompython 
+#   Aula 14 - Algoritmo "Verbo To Be"
+#
+###############################################################################
+
 def verbo_to_be_futuro_simples(pronome):
   return pronome + " will" + " be"
   
